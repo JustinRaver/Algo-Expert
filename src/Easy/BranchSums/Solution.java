dc0a5447c247e19159803ebf0a8e3dba3d59b0ea
Justin Raver <48226937+JustinRaver@users.noreply.github.com>
2021-10-11 00:40:17 -0600

    Added Branch Sums problem description

--- src/Easy/BranchSums/Solution.java
+++ src/Easy/BranchSums/Solution.java
@@ -4,6 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
+    /*
+     * Write a function that takes in a Binary Tree and returns a list of its branch sums
+     * ordered from the leftmost branch sum to a rightmost branch sum.
+     *
+     * A branch sum is the sum of all values in a Binary Tree branch. A Binary Tree Branch
+     * is a path of nodes in a tree that starts at the root node and ends at any leafy node.
+     *
+     * Each BinaryTree node has an integer value, a left child node, and a right child node.
+     * Children nodes can either be a BinaryTree nodes themselves or None/Null.
+     */
     public static class BinaryTree {
         int value;
         BinaryTree left;
